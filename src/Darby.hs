030c65b991121c958f3dd93d48511e43378a7222
Lúcás Meier <cronokirby@gmail.com>
2018-11-06 21:55:14 +0100

    Add a Context type and monad for it
    Uses optparse-applicative to parse a Context,
    and a monad for working with it.

--- src/Darby.hs
+++ src/Darby.hs
@@ -3,5 +3,13 @@ module Darby
     )
 where
 
+import Darby.Context (ContextM, runCLIContextM)
+
+
 runDarby :: IO ()
-runDarby = putTextLn "Running Darby..."
+runDarby = runCLIContextM darby
+
+
+-- | Entry point into the main program
+darby :: ContextM ()
+darby = putTextLn "Running Darby..."
---
+++ src/Darby/Context.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-|
+Description: Contains a Context, and a CL arg parser for it
+
+Also exports a ContextM monad for having a computation
+around this context.
+-}
+module Darby.Context
+   ( Context(..)
+   , argsParser
+   , ContextM(..)
+   , runCLIContextM
+   )
+where
+
+import Options.Applicative
+
+
+{- | Contains the Context for the Darby program.
+
+This is currently restricted to just the directory
+to shuffle, but will be expanded later.
+-}
+newtype Context = Context
+    { contextDirectory :: FilePath
+    }
+
+
+contextParser :: Parser Context
+contextParser = Context
+    <$> strArgument (metavar "DIRECTORY")
+
+argsParser :: ParserInfo Context
+argsParser = info (contextParser <**> helper)
+    (  fullDesc
+    <> progDesc "Shuffle and play the songs in FILE"
+    )
+
+
+-- | Represents a computation with access to a Context
+newtype ContextM a = ContextM
+    { getContextM :: ReaderT Context IO a
+    }
+    deriving ( Functor, Applicative, Monad
+             , MonadReader Context, MonadIO
+             )
+
+-- | Parses command line arguments to run a ContextM
+runCLIContextM :: ContextM a -> IO a
+runCLIContextM (ContextM m) =
+    execParser argsParser >>= runReaderT m
