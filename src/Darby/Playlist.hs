7b907b6032704f671aadd98f803984a40d1f7f87
Lúcás Meier <cronokirby@gmail.com>
2018-11-07 15:02:03 +0100

    Add shuffle functionality for playlists

--- src/Darby/Playlist.hs
+++ src/Darby/Playlist.hs
@@ -12,7 +12,10 @@ where
 
 import Relude
 
+import Data.Array.IO (IOArray, newListArray, 
+                      readArray, writeArray)
 import System.Directory (listDirectory)
+import System.Random (randomRIO)
 
 
 -- | Represents a song which can be played
@@ -25,7 +28,29 @@ newtype PlayList = PlayList [Song]
 isMP3File :: FilePath -> Bool
 isMP3File str = isPrefixOf "3pm." (reverse str)
 
+{- | Reads a playlist from a base directory
+
+This works by listing all files in the directory,
+and filtering out everything that isn't an mp3 file.
+This should be extended in the future to all music
+files that we support.
+-}
 readPlaylist :: MonadIO m => FilePath -> m PlayList
 readPlaylist dir =
     PlayList . map Song . filter isMP3File
     <$> liftIO (listDirectory dir)
+
+
+shuffle :: MonadIO m => PlayList -> m PlayList
+shuffle (PlayList songs) = fmap PlayList . liftIO $ do
+    arr <- newArray ln songs
+    forM [1..ln] $ \i -> do
+        j  <- randomRIO (i, ln)
+        vi <- readArray arr i
+        vj <- readArray arr j
+        writeArray arr j vi
+        return vj
+  where
+    ln = length songs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n xs = liftIO $ newListArray (1, n) xs
